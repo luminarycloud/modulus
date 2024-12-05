df3d92144e73f75b7c7a57d5466cc3641b7eb974
Michael Mara <mmara@luminarycloud.com>
2024-12-05 17:46:51 +0000

    Extra jobs option

--- examples/cfd/external_aerodynamics/xaeronet/download_aws_dataset.sh
+++ examples/cfd/external_aerodynamics/xaeronet/download_aws_dataset.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-# Usage: ./script.sh -d <local_directory> [-s]
+# Usage: ./script.sh -d <local_directory> [-s] [-j <concurrent_downloads>]
 # Use -s flag to skip downloading volume files (.vtu)
+# Use -j to specify number of concurrent downloads (default: 8)
+
+CONCURRENT_JOBS=8  # Default value
 SKIP_VOLUME=false
-while getopts "d:s" opt; do
+
+while getopts "d:sj:" opt; do
     case $opt in
         d) LOCAL_DIR="$OPTARG";;
         s) SKIP_VOLUME=true;;
+        j) CONCURRENT_JOBS="$OPTARG";;
         \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
     esac
 done
 
 if [ -z "$LOCAL_DIR" ]; then
     echo "Error: Local directory (-d) must be specified"
-    echo "Usage: $0 -d <local_directory> [-s]"
+    echo "Usage: $0 -d <local_directory> [-s] [-j <concurrent_downloads>]"
     echo "  -d: Specify local directory for downloads"
     echo "  -s: Skip downloading volume files (optional)"
+    echo "  -j: Number of concurrent downloads (default: 8)"
     exit 1
 fi
 
@@ -33,16 +39,20 @@ download_run_files() {
     # Download volume file if not skipped
     if [ "$SKIP_VOLUME" = false ]; then
         if [ ! -f "$RUN_LOCAL_DIR/volume_$i.vtu" ]; then
+            echo "Downloading volume_$i.vtu..."
             aws s3 cp --no-sign-request "s3://$S3_BUCKET/$S3_PREFIX/$RUN_DIR/volume_$i.vtu" "$RUN_LOCAL_DIR/" &
-       fi
+        fi
     fi
 
-    # Download STL and VTP files
+    # Download STL file
     if [ ! -f "$RUN_LOCAL_DIR/drivaer_$i.stl" ]; then
+        echo "Downloading drivaer_$i.stl..."
         aws s3 cp --no-sign-request "s3://$S3_BUCKET/$S3_PREFIX/$RUN_DIR/drivaer_$i.stl" "$RUN_LOCAL_DIR/" &
     fi
 
+    # Download VTP file
     if [ ! -f "$RUN_LOCAL_DIR/boundary_$i.vtp" ]; then
+        echo "Downloading boundary_$i.vtp..."
         aws s3 cp --no-sign-request "s3://$S3_BUCKET/$S3_PREFIX/$RUN_DIR/boundary_$i.vtp" "$RUN_LOCAL_DIR/" &
     fi
 
@@ -56,11 +66,15 @@ download_run_files() {
     wait
 }
 
+echo "Starting downloads with $CONCURRENT_JOBS concurrent jobs..."
+
 for i in $(seq 1 500); do
     download_run_files "$i" &
-    if (( $(jobs -r | wc -l) >= 8 )); then
+    if (( $(jobs -r | wc -l) >= CONCURRENT_JOBS )); then
         wait -n
     fi
 done
 
-wait 
+wait
+
+echo "All downloads completed!"
