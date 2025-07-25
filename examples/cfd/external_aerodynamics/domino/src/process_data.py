440ef8a9080086a002fbaf5b3cd0bd0cfa123c93
Michael Mara <mmara@luminarycloud.com>
2025-07-25 00:02:16 +0000

    Process data and openfoam datapipe functionality

--- examples/cfd/external_aerodynamics/domino/src/process_data.py
+++ examples/cfd/external_aerodynamics/domino/src/process_data.py
@@ -25,7 +25,7 @@ from openfoam_datapipe import OpenFoamDataset
 from physicsnemo.utils.domino.utils import *
 import multiprocessing
 import hydra, time
-from hydra.utils import to_absolute_path
+import numbers
 from omegaconf import DictConfig, OmegaConf
 
 
@@ -54,7 +54,6 @@ def process_files(*args_list):
 @hydra.main(version_base="1.3", config_path="conf", config_name="config")
 def main(cfg: DictConfig):
     print(f"Config summary:\n{OmegaConf.to_yaml(cfg, sort_keys=True)}")
-    phase = "train"
     volume_variable_names = list(cfg.variables.volume.solution.keys())
     num_vol_vars = 0
     for j in volume_variable_names:
@@ -77,6 +76,15 @@ def main(cfg: DictConfig):
         name: cfg.variables.global_parameters[name]["reference"]
         for name in global_params_names
     }
+
+    epsilon = 1e-10  # Normalization will fail if reference is too close to zero
+    for name, value in global_params_reference.items():
+        if isinstance(value, numbers.Number):
+            if abs(value) <= epsilon:
+                raise ValueError(
+                    f"Global parameter '{name}' has a reference value of {value}, which is too close to zero."
+                )
+
     global_params_types = {
         name: cfg.variables.global_parameters[name]["type"]
         for name in global_params_names
@@ -90,6 +98,7 @@ def main(cfg: DictConfig):
         global_params_types=global_params_types,
         global_params_reference=global_params_reference,
         model_type=cfg.model.model_type,
+        stl_suffix=cfg.data.stl_suffix,
     )
     output_dir = cfg.data_processor.output_dir
     create_directory(output_dir)
